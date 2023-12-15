b45538619c780b1f6b36a03424de4ba5618f4d8e
Timur <hrynenko.timur@gmail.com>
2023-12-15 17:35:06 +0200

    fix: card animation fixed

--- src/components/PokemonCard/PokemonCard.styles.tsx
+++ src/components/PokemonCard/PokemonCard.styles.tsx
@@ -2,11 +2,13 @@ import { Card } from "@mui/material";
 import styled from "styled-components";
 
 export const CardBody = styled(Card)`
-  cursor: pointer;
-  transition: all 0.4s;
-  &:hover {
-    transform: scale(1.07) translateY(-15px);
-    box-shadow: 0px 5px 10px grey;
+  &.MuiPaper-root {
+    cursor: pointer;
+    transition: all 0.3s;
+    &:hover {
+      transform: scale(1.07) translateY(-15px);
+      box-shadow: 0px 5px 10px grey;
+    }
   }
 `;
 
